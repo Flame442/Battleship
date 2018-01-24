f3568f369d985b5471133b0277e019666ca83837
Flame442 <34169552+Flame442@users.noreply.github.com>
2018-01-24 10:16:14 -0500

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,9 +1,7 @@
 name = ['','']
-print('Welcome to Battleship!')
-for a in range(2): name[a] = input('Name for player '+str(a+1)+'?\n')
-
 board = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 let = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+letnum = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
 bkey = [{0:'· ',1:'O ',2:'X ',3:'· '},{0:'· ',1:'O ',2:'X ',3:'# '}]
 pswap = {1:0,0:1}
 #   KEY FOR BOARD # TO SYMBOL:
@@ -17,6 +15,8 @@ pswap = {1:0,0:1}
 #   1: Miss Spot
 #   2: Hit Spot
 #   3: Unchecked Ship Spot
+def clear(): 
+  for c in range(50): print()
 def bprint(player,bt):
   b = '  '
   for z in range(10): b += let[z]+' '
@@ -25,5 +25,51 @@ def bprint(player,bt):
     a = (str(y)+' ')
     for x in range(10): a += bkey[bt][board[player][(y*10)+x]]
     print(a)
+def place(player,length):
+  i = 0
+  while i == 0:
+    t = input()
+    try:
+      x = letnum[t[0]]
+      y = int(t[1])
+      d = t[2]
+      if d == 'r':
+        f = 0
+        for z in range(length):
+          if board[player][(y*10)+x+z] != 0:
+            f = 1
+        if f == 1:
+          1 / 0
+        for z in range(length):
+          board[player][(y*10)+x+z] = 3
+      elif d == 'd':
+        for z in range(length):
+          if board[player][((y+z)*10)+x] != 0:
+            f = 1
+        if f == 1:
+          1 / 0
+        for z in range(length):
+          board[player][((y+z)*10)+x] = 3
+    except:
+      print('Invalid input')
 
-print(name[0]+', it is your turn to set up your ships')
+print('Welcome to Battleship!')
+for a in range(2): name[a] = input('Name for player '+str(a+1)+'?\n')
+clear()
+print(name[0]+', it is your turn to set up your ships. Place ships by entering the top left cord in xyd format.')
+bprint(0,1)
+print('Place your 5 length ship')
+place(0,5)
+bprint(0,1)
+print('Place your 4 length ship')
+place(0,4)
+bprint(0,1)
+print('Place your first 3 length ship')
+place(0,3)
+bprint(0,1)
+print('Place your second 3 length ship')
+place(0,3)
+bprint(0,1)
+print('Place your 2 length ship')
+place(0,2)
+bprint(0,1)
